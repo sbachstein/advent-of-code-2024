370a767a76351782d249e2aae92ad46409830317
Simon <mail@sbachstein.de>
2024-12-20 12:13:12 +0100

    Day 20

---
+++ day-20/src/part1.rs
@@ -0,0 +1,140 @@
+use crate::custom_error::AocError;
+use glam::IVec2;
+use pathfinding::prelude::dijkstra;
+use std::collections::HashSet;
+use std::io::Error;
+
+struct Maze {
+    walls: HashSet<IVec2>,
+    start: IVec2,
+    end: IVec2,
+}
+
+#[derive(PartialEq, Eq, Clone, Copy, Hash)]
+enum Direction {
+    North,
+    South,
+    West,
+    East,
+}
+
+const DIRECTIONS: [Direction; 4] = [
+    Direction::North,
+    Direction::South,
+    Direction::West,
+    Direction::East,
+];
+
+impl From<Direction> for IVec2 {
+    fn from(dir: Direction) -> IVec2 {
+        match dir {
+            Direction::North => IVec2::new(0, -1),
+            Direction::South => IVec2::new(0, 1),
+            Direction::West => IVec2::new(-1, 0),
+            Direction::East => IVec2::new(1, 0),
+        }
+    }
+}
+
+fn parse(input: &str) -> Result<Maze, AocError> {
+    let mut walls = HashSet::new();
+    let mut start: Option<IVec2> = None;
+    let mut end: Option<IVec2> = None;
+
+    for (y, line) in input.lines().enumerate() {
+        for (x, c) in line.chars().enumerate() {
+            match c {
+                '#' => {
+                    walls.insert(IVec2::new(x as i32, y as i32));
+                }
+                'S' => {
+                    start = Some(IVec2::new(x as i32, y as i32));
+                }
+                'E' => {
+                    end = Some(IVec2::new(x as i32, y as i32));
+                }
+                _ => {}
+            }
+        }
+    }
+
+    Ok(Maze {
+        walls,
+        start: start.ok_or(Error::other("No starting position"))?,
+        end: end.ok_or(Error::other("No starting position"))?,
+    })
+}
+
+#[tracing::instrument]
+pub fn process(_input: &str, minimum_saving: usize) -> miette::Result<String, AocError> {
+    let maze = parse(_input)?;
+
+    let normal_path = dijkstra(
+        &maze.start,
+        |pos| {
+            DIRECTIONS
+                .iter()
+                .map(|&dir| (pos + IVec2::from(dir), 1))
+                .filter(|(pos, _cost)| !maze.walls.contains(pos))
+                .collect::<Vec<(IVec2, usize)>>()
+        },
+        |pos| *pos == maze.end,
+    )
+    .ok_or(Error::other("No path found"))?;
+
+    let normal_time = normal_path.1;
+
+    let cheated_path_times = normal_path
+        .0
+        .iter()
+        .enumerate()
+        .flat_map(|(previous_steps, &pos)| {
+            DIRECTIONS.iter().filter_map({
+                let normal_path_nodes = normal_path.0.clone();
+                move |&dir| {
+                    let cheated_pos = pos + 2 * IVec2::from(dir);
+                    normal_path_nodes
+                        .iter()
+                        .position(|pos| *pos == cheated_pos)
+                        .filter(|back_on_track_pos| *back_on_track_pos > previous_steps + 2)
+                        .map(|back_on_track_pos| {
+                            normal_time - back_on_track_pos + previous_steps + 2
+                        })
+                }
+            })
+        })
+        .collect::<Vec<usize>>();
+
+    let num_cheated_paths_with_minimum_savings = cheated_path_times
+        .iter()
+        .filter(|&&time| time <= normal_time - minimum_saving)
+        .count();
+
+    Ok(num_cheated_paths_with_minimum_savings.to_string())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_process() -> miette::Result<()> {
+        let input = "###############
+#...#...#.....#
+#.#.#.#.#.###.#
+#S#...#.#.#...#
+#######.#.#.###
+#######.#.#...#
+#######.#.###.#
+###..E#...#...#
+###.#######.###
+#...###...#...#
+#.#####.#.###.#
+#.#...#.#.#...#
+#.#.#.#.#.#.###
+#...#...#...###
+###############";
+        assert_eq!("44", process(input, 1)?);
+        Ok(())
+    }
+}
